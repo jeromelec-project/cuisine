c823bebcce8dc9d6e87a2695da95bcba221dfc72
jeromelec-project <jerome.lecapitaine@institutsolacroup.com>
2026-02-13 09:20:01 +0100

    supression recette

--- script.js
+++ script.js
@@ -13,26 +13,51 @@
 const inputRecetteNom = document.getElementById("inputRecetteNom");
 const inputRecetteIngredients = document.getElementById("inputRecetteIngredients");
 const selectTypeRecette = document.getElementById("selectTypeRecette");
-const formRecette = document.querySelector(".formRecette");
-const tableRecettes = document.querySelector(".tableRecettes");
+const FormRecette = document.getElementById("FormRecette");
+const tableRecettes = document.getElementById("tableRecettes");
+
+
+function removeRecette(recetteId) {
+    recettes = recettes.filter((recette) => recette.id !== recetteId);
+    localStorage.setItem("recettes", JSON.stringify(recettes));
+    tableauRecettes();
+}
 
 
 
-const recettes = [];
 
+//console.log(FormRecette);
+if(!localStorage.getItem("recettes")) {
+    var recettes = [];
+}else{
+    var recettes = JSON.parse(localStorage.getItem("recettes"));
+}
 
 function afficherRecettesTr() {
-let tr = "<tr><td>lol</td><td>lol2</td><td></td><td></td><td>lol12000</td></tr>";
-localStorage.setItem("ContenuTableauRecettes", tr);
+console.log("recetes:",recettes);
+
+let tr = recettes.map((recette) => {
+    return `<tr>
+    <td>${recette.id}</td>
+    <td>${recette.nom}</td>
+    <td>${recette.ingredients}</td>
+    <td>${recette.type}</td>
+    <td><button type="submit" name="recetteModifier" onclick="modifierRecette(${recette.id})" value="${recette.id}">Modifier</button>
+    <button type="submit" name="recetteSupprimer" onclick="removeRecette(${recette.id})" value="${recette.id}">Supprimer</button>
+    <button type="submit" name="recetteAjouter" onclick="favoritesRecettes(${recette.id})" value="${recette.id}">Ajouter aux favoris</button></td>
+    </tr>`
+})
+const trjoin = tr.join(" ");
+localStorage.setItem("ContenuTableauRecettes", trjoin);
 }
 
 
 function tableauRecettes() {
     afficherRecettesTr();
     const tr = localStorage.getItem("ContenuTableauRecettes");
-    console.log(tr);
+    //console.log(tr);
 
-const tableaulocal = tableRecettes.innerHtml = `<table>
+    const tableauLocal = tableRecettes.innerHTML = `<table class="tableauRecettes">
     <tr>
         <th>Id</th>
         <th>Nom</th>
@@ -40,16 +65,19 @@ const tableaulocal = tableRecettes.innerHtml = `<table>
         <th>type</th>
         <th>Actions</th>
     </tr>
-${tr}
-</table>`;
-     return tableaulocal;   
+    ${tr}
+    </table>`;
+     //console.log(tableauLocal); 
+
+    return tableauLocal;  
 }
 
+    tableauRecettes();
 
-if (formRecette) {
-    
 
-formRecette.addEventListener("submit", (event) => {
+
+
+FormRecette.addEventListener("submit", (event) => {
     event.preventDefault();
     const recette = {
         nom: inputRecetteNom.value,
@@ -60,11 +88,11 @@ formRecette.addEventListener("submit", (event) => {
         ...recette,
         id: recettes.length + 1,
     });
-    console.log(recettes);
+    console.log("recettes:",recettes);
     localStorage.setItem("recettes", JSON.stringify(recettes));
 
     tableauRecettes();
+
 });
 
 
-}
